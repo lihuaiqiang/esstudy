e761ddaf802ea3f2de4cfb9565f2f2ef427056d2
lihq <lihq@primeton.com>
2022-07-08 11:19:48 +0800

    es集成Spark没能成功！！

--- es-spark-streaming/src/main/java/com/atguigu/es/SparkStreamingESTest.scala
+++ es-spark-streaming/src/main/java/com/atguigu/es/SparkStreamingESTest.scala
@@ -8,41 +8,45 @@ import org.elasticsearch.action.index.{IndexRequest, IndexResponse}
 import org.elasticsearch.client.{RequestOptions, RestClient, RestHighLevelClient}
 import org.elasticsearch.common.xcontent.XContentType
 
+/**
+  * TODO 集成 Spark 测试没能成功
+  */
 object SparkStreamingESTest {
 
-    def main(args: Array[String]): Unit = {
+  // todo 集成 Spark 测试没能成功。原因是在cmd 窗口执行 nc -lp 9999 命令后瞬间退出。然后启动本主方法也就没能启动成功
+  def main(args: Array[String]): Unit = {
 
-        val sparkConf = new SparkConf().setMaster("local[*]").setAppName("ESTest")
-        val ssc = new StreamingContext(sparkConf, Seconds(3))
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("ESTest")
+    val ssc = new StreamingContext(sparkConf, Seconds(3))
 
-        val ds: ReceiverInputDStream[String] = ssc.socketTextStream("localhost", 9999)
-        ds.foreachRDD(
-            rdd => {
-                rdd.foreach(
-                    data => {
-                        val client = new RestHighLevelClient(
-                            RestClient.builder(new HttpHost("localhost",9200, "http"))
-                        )
+    val ds: ReceiverInputDStream[String] = ssc.socketTextStream("localhost", 9999)
+    ds.foreachRDD(
+      rdd => {
+        rdd.foreach(
+          data => {
+            val client = new RestHighLevelClient(
+              RestClient.builder(new HttpHost("localhost",9200, "http"))
+            )
 
-                        val ss = data.split(" ")
+            val ss = data.split(" ")
 
-                        val request = new IndexRequest()
-                        request.index("product").id(ss(0))
-                        val json =
-                            s"""
-                              | {  "data" : "${ss(1)}" }
-                              |""".stripMargin
-                        request.source(json, XContentType.JSON)
+            val request = new IndexRequest()
+            request.index("product").id(ss(0))
+            val json =
+              s"""
+                 | {  "data" : "${ss(1)}" }
+                 |""".stripMargin
+            request.source(json, XContentType.JSON)
 
-                        val response: IndexResponse = client.index(request, RequestOptions.DEFAULT)
-                        println(response.getResult)
-                        client.close()
-                    }
-                )
-            }
+            val response: IndexResponse = client.index(request, RequestOptions.DEFAULT)
+            println(response.getResult)
+            client.close()
+          }
         )
+      }
+    )
 
-        ssc.start()
-        ssc.awaitTermination()
-    }
+    ssc.start()
+    ssc.awaitTermination()
+  }
 }
